fdf5a091b36ed567092840ff13f159b073c4babe
Rio-Sh <shrio.wr@google.com>
2023-06-12 23:50:10 +0900

    Initialize Repository

---
+++ app/src/main/java/com/github/rio_sh/mareave/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.rio_sh.mareave
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
