975de994915fa86f355cca51fa4c90799899c99b
Rio-Sh <shrio.wr@gmail.com>
2023-06-20 11:18:32 +0900

    Setup Map and AR view

--- app/src/main/java/com/github/rio_sh/mareave/MainActivity.kt
+++ app/src/main/java/com/github/rio_sh/mareave/MainActivity.kt
@@ -2,10 +2,93 @@ package com.github.rio_sh.mareave
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.github.rio_sh.mareave.common.helpers.FullScreenHelper
+import com.github.rio_sh.mareave.common.samplerender.SampleRender
+import com.github.rio_sh.mareave.helpers.MainView
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.core.codelabs.hellogeospatial.helpers.ARCoreSessionLifecycleHelper
+import com.google.ar.core.codelabs.hellogeospatial.helpers.GeoPermissionsHelper
+import com.google.ar.core.exceptions.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    lateinit var arCoreSessionHelper: ARCoreSessionLifecycleHelper
+    lateinit var view: MainView
+    lateinit var renderer: GeoRenderer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        arCoreSessionHelper = ARCoreSessionLifecycleHelper(this)
+        arCoreSessionHelper.exceptionCallback =
+            { exception ->
+                val message =
+                    when (exception) {
+                        is UnavailableUserDeclinedInstallationException ->
+                            "Please install Google Play Services for AR"
+                        is UnavailableApkTooOldException -> "Please update ARCore"
+                        is UnavailableSdkTooOldException -> "Please update this app"
+                        is UnavailableDeviceNotCompatibleException -> "This device does not support AR"
+                        is CameraNotAvailableException -> "Camera not available. Try restarting the app."
+                        else -> "Failed to create AR session: $exception"
+                    }
+                Log.e(TAG, "ARCore threw an exception", exception)
+                view.snackbarHelper.showError(this, message)
+            }
+
+        // Set up Session
+        arCoreSessionHelper.beforeSessionResume = ::configureSession
+        lifecycle.addObserver(arCoreSessionHelper)
+
+        // Set up UI
+        renderer = GeoRenderer(this)
+        lifecycle.addObserver(renderer)
+
+        // Set up UI
+        view = MainView(this)
+        lifecycle.addObserver(view)
+        setContentView(view.root)
+
+        SampleRender(view.surfaceView, renderer, assets)
+
+    }
+
+    //Configure ARCore session. While The GeospatialModel is Enabled, the application can obtain
+    // Geospatial information.
+    fun configureSession(session: Session) {
+        session.configure(
+            session.config.apply {
+                geospatialMode = Config.GeospatialMode.ENABLED
+            }
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!GeoPermissionsHelper.hasGeoPermissions(this)) {
+            Toast.makeText(this,
+                "Camera and location permissions are needed to run this application",
+                Toast.LENGTH_LONG)
+                .show()
+            if(!GeoPermissionsHelper.shouldShowRequestPermissionRationale(this)) {
+                GeoPermissionsHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus)
     }
 }
